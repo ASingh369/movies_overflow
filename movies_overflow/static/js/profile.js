f22dfd28fcce9d0c8873f844c1e7f666354a491e
Amrit Pal Singh <amritpalsinghs369@gmail.com>
2019-06-25 20:12:20 -0700

    show movies in modal

--- movies_overflow/static/js/profile.js
+++ movies_overflow/static/js/profile.js
@@ -1,7 +1,34 @@
 const profileBgImage = document.getElementById("profile-bg");
 const profileMovies = document.getElementById("profile-movies");
+const editListBtn = document.getElementById("edit-list-btn");
+const editList = document.getElementById("edit-list");
+var movies_list = []; // Top 10 movies array
 
 displayTop10();
+editListBtn.addEventListener('click', showCurrentTop10Movies);
+
+function showCurrentTop10Movies(){
+  console.log(movies_list);
+  editList.innerHTML = "";
+  movies_list.forEach(function(movie){
+    const div = document.createElement("div");
+    div.className = "d-flex";
+    div.innerHTML = `
+    <div class="rank">${movie[1]}</div>
+      <div class="name">${movie[0].movie.title}</div>
+      <div class="buttons d-flex">
+        <div class="edit d-flex">
+          <div class="move-up"><i class="fas fa-sort-numeric-up"></i></div>
+          <div class="move-down"><i class="fas fa-sort-numeric-down"></i></div>
+        </div>
+        <div class="delete-box">
+          <span class="delete"><i class="far fa-trash-alt"></i></span>
+        </div>
+    </div>
+    `;
+    editList.appendChild(div);
+  });
+}
 
 function sortMoviesList(arr){
   for (var i = 0; i < arr.length; i++){
@@ -26,7 +53,6 @@ function displayTop10(){
     if (this.status === 200){
       top10_movies = JSON.parse(this.responseText);
       total_movies = top10_movies.length;
-      var movies_list = [];
       top10_movies.forEach(function(top10_movie){
 
         tmdb.getJustMovie(top10_movie.movie_id).then(data => {
@@ -39,7 +65,7 @@ function displayTop10(){
                 const movieCard = document.createElement("div");
                 movieCard.className = "movie-card-padding";
                 show0 = "0";
-                if (top10_movie.rank != 0){
+                if (movie[1] == 10){
                   show0 = "";
                 }
                 movieCard.innerHTML = `
@@ -66,7 +92,6 @@ function displayTop10(){
                 profileBgImage.src = `https://image.tmdb.org/t/p/w1280/${movies_list[0][0].movie.backdrop_path}`;
               }
             }
-            
           }
         });
       });
